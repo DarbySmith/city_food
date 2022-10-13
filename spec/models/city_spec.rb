c2db878b8354d5990771161a9ce6e2e7bd75296f
Darby Smith <newman.darby2@gmail.com>
2022-10-13 16:07:01 -0400

    Test city association

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe City, type: :model do
+  before :each do
+    @braselton = City.create!(name:'Braselton', population:12178, metropolis:false)
+    @atlanta = City.create!(name:'Atlanta', population:49762, metropolis:true)
+    @fogo = @atlanta.restaurants.create!(name: 'Fodo de Chao', food_type: 'steak house', alcohol_served: true, rating: 5)
+    @jacks = @braselton.restaurants.create!(name: 'Jacks Public House', food_type:'American', alcohol_served: true, rating: 5)
+  end
+  
+  describe 'validations' do
+    it {should have_many :restaurants}
+  end
+end

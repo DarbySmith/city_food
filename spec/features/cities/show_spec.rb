0d3b8551a68795fc53974863ded6570e2e9faf2d
Darby Smith <newman.darby2@gmail.com>
2022-10-13 10:58:47 -0400

    Test pass for index and show feat

---
+++ spec/features/cities/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'shows index of all cities' do
+  it 'shows a city with attributes' do
+    braselton = City.create!(name:'Braselton', population:12178, metropolis:false)
+    atlanta = City.create!(name:'Atlanta', population:49762, metropolis:true)
+
+    visit "/cities/#{braselton.id}"
+
+    expect(page).to have_content(braselton.name)
+    expect(page).to have_content(braselton.population)
+    expect(page).to have_content(braselton.metropolis)
+  end
+end

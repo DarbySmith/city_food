0de157dc5282a0bfbd63a4432450649c67ec564f
Darby Smith <newman.darby2@gmail.com>
2022-10-14 17:45:00 -0400

    Test order of cities displayed

--- spec/features/cities/index_spec.rb
+++ spec/features/cities/index_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe 'shows index of all cities' do
     expect(page).to have_content(braselton.name)
     expect(page).to have_content(atlanta.name)
   end
+
+  it 'shows cities in order of most recent created first' do
+    braselton = City.create!(name:'Braselton', population:12178, metropolis:false)
+    atlanta = City.create!(name:'Atlanta', population:49762, metropolis:true)
+
+    visit '/cities'
+
+    # this = find('.this')
+    # that = find('.that')
+
+    expect(braselton.name).to appear_before(atlanta.name)
+  end
 end

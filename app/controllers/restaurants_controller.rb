a78bf2c27720e929849ba82de4c1465dec4336e4
Darby Smith <newman.darby2@gmail.com>
2022-10-16 19:58:29 -0400

    Feat: add restaurant link and form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,19 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @city = City.find(params[:city_id])
+  end
+
+  def create
+    city = City.find(params[:city_id])
+    restaurant = city.restaurants.create!(restaurant_params)
+    redirect_to "/cities/#{restaurant.city_id}/restaurants"
+  end
+
+  private
+  def restaurant_params
+    params.permit(:name, :food_type, :alcohol_served, :rating)
+  end
 end

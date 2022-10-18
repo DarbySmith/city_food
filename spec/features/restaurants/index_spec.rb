e14ee38d533aa9777d1bb5dc4e468ee3fc32621b
Darby Smith <newman.darby2@gmail.com>
2022-10-17 20:42:54 -0400

    Test for update restaurant on index and cities rest

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -36,4 +36,27 @@ RSpec.describe 'shows index of all restaurants' do
 
     expect(page).to_not have_content("Zaxbys")
   end
+
+  it 'has a link to update the restaurant' do
+    braselton = City.create!(name:'Braselton', population:12178, metropolis:false)
+    atlanta = City.create!(name:'Atlanta', population:49762, metropolis:true)
+    fogo = atlanta.restaurants.create!(name: 'Fogo de Chao', food_type: 'steak house', alcohol_served: true, rating: 5)
+    jacks = braselton.restaurants.create!(name: 'Jacks Public House', food_type:'American', alcohol_served: true, rating: 5)
+
+    visit "/restaurants"
+
+    click_link "Update #{fogo.name}"
+
+    expect(current_path).to eq("/restaurants/#{fogo.id}/edit")
+    fill_in :name, with: "Fogo de Chao"
+    fill_in :food_type, with: "Steakhouse"
+    fill_in :alcohol_served, with: true
+    fill_in :rating, with: 5
+
+    click_on "Update Restaurant"
+
+    expect(current_path).to eq("/restaurants/#{fogo.id}")
+    expect(page).to have_content("Steakhouse")
+    expect(page).to_not have_content("steak house")
+  end
 end
